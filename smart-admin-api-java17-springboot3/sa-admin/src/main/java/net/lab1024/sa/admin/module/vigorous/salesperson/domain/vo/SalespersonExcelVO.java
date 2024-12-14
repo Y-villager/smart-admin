cfb8fa9af751a72b16f69748bae94a51281c1a64
Y-villager <59049169+Y-villager@users.noreply.github.com>
2024-12-14 16:24:16 +0800

    导入导出

--- smart-admin-api-java17-springboot3/sa-admin/src/main/java/net/lab1024/sa/admin/module/vigorous/salesperson/domain/vo/SalespersonExcelVO.java
+++ smart-admin-api-java17-springboot3/sa-admin/src/main/java/net/lab1024/sa/admin/module/vigorous/salesperson/domain/vo/SalespersonExcelVO.java
@@ -12,17 +12,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SalespersonExcelVO {
-//    @ExcelProperty("业务员编码")
-//    private String salespersonCode;
-//
-//    @ExcelProperty("业务员名称")
-//    private String salespersonName;
-//
-//    @ExcelProperty("职位")
-//    private String position;
-//
-//    @ExcelProperty("部门")
-//    private String department;
+    @ExcelProperty("业务员编码")
+    private String salespersonCode;
+
+    @ExcelProperty("业务员名称")
+    private String salespersonName;
+
+    @ExcelProperty("职位")
+    private String position;
+
+    @ExcelProperty("部门")
+    private String department;
 
 
 }
